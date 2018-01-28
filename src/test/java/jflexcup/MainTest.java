3df40ab964ec7f2f64d80931c987f41ac266cc9c
Fredy Wijaya <fredy.wijaya@gmail.com>
2018-01-28 12:59:06 -0600

    Add more test cases

--- src/test/java/jflexcup/MainTest.java
+++ src/test/java/jflexcup/MainTest.java
@@ -2,12 +2,11 @@ package jflexcup;
 
 import org.junit.Test;
 
-import java.io.File;
 import java.io.Reader;
 import java.io.StringReader;
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
     @Test
@@ -21,6 +20,9 @@ public class MainTest {
         assertEquals(new BigDecimal(".45e1"), Main.parse(reader("NUMBER .45e1")));
         assertEquals(new BigDecimal(".45e-1"), Main.parse(reader("NUMBER .45e-1")));
         assertEquals("hello.123_world", Main.parse(reader("NAME hello.`123_world`")));
+        assertEquals("123_world", Main.parse(reader("NAME 123_world")));
+        assertEquals("hello.123_world", Main.parse(reader("NAME hello. 123_world")));
+//        assertEquals("hello.123_world", Main.parse(reader("NAME hello.123_world")));
     }
 
     private static Reader reader(String str) {
